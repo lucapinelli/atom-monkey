b071510ae28d921b7378c6435cd8310099f424b0
Luca Pinelli <pinelli.luca@gmail.com>
2022-03-27 23:41:42 +0200

    improve validation error (wip)

--- spec/atom-monkey-util-json-spec.js
+++ spec/atom-monkey-util-json-spec.js
@@ -29,9 +29,109 @@ describe('util/json', () => {
     })
 
     it('returns success = false and an error message if the json is not valid', () => {
-      const { success, error } = formatJson('{"ok", true}')
-      expect(success).toBe(false)
-      expect(error).toBeTruthy()
+      {
+        const json =
+        `{
+          "project": {
+            "name": "atom-monkey",
+            "description": "If the monkey can do it, you don't have to :P"
+          }
+          "ok": false
+        }`
+        const { success, error, rowPosition, focusBox } = formatJson(json)
+        expect(success).toBe(false)
+        expect(error).toEqual('Unexpected string in JSON at position 157')
+        expect(rowPosition).toBeTruthy()
+        expect(rowPosition.row).toEqual(6)
+        expect(rowPosition.index).toEqual(10)
+        expect(focusBox).toEqual([
+          '            "description": "If the monkey can do i…',
+          '          }                                        ',
+          '          "ok": false                              ',
+          '----------^----------------------------------------',
+          '        }                                          '
+        ].map(line => line.trimEnd()).join('\n'))
+      }
+
+      {
+        const json = `
+        {
+          "project": {
+            "name": "atom-monkey",
+            "description": "If the monkey can do it, "you" don't have to :P",
+          },
+          "ok": false
+        }`
+        const { success, error, rowPosition, focusBox } = formatJson(json)
+        expect(success).toBe(false)
+        expect(error).toEqual('Unexpected token y in JSON at position 123')
+        expect(rowPosition).toBeTruthy()
+        expect(rowPosition.row).toEqual(5)
+        expect(rowPosition.index).toEqual(54)
+        expect(focusBox).toEqual([
+          '…ect": {                                                       ',
+          '…me": "atom-monkey",                                           ',
+          '…scription": "If the monkey can do it, "you" don\'t have to :P",',
+          '----------------------------------------^----------------------',
+          '…                                                              ',
+          '… false                                                        '
+        ].map(line => line.trimEnd()).join('\n'))
+      }
+
+      {
+        const json = `
+        {
+          "project": {
+            "name": "atom-monkey",
+            "description": "If the monkey can do it, you don't have to :P\\n\\nThe plugin will add the following functionalities ... bla bla bla ...",
+            "tags": ["atom", "plugin", "uuid", "date", "iso" "json", "validate", "format"]
+          },
+          "ok": false
+        }`
+        const { success, error, rowPosition, focusBox } = formatJson(json)
+        expect(success).toBe(false)
+        expect(error).toEqual('Unexpected string in JSON at position 279')
+        expect(rowPosition).toBeTruthy()
+        expect(rowPosition.row).toEqual(6)
+        expect(rowPosition.index).toEqual(61)
+        expect(focusBox).toEqual([
+          '…tom-monkey",                                                                    ',
+          '…on": "If the monkey can do it, you don\'t have to :P\\n\\nThe plugin will add the …',
+          '…atom", "plugin", "uuid", "date", "iso" "json", "validate", "format"]            ',
+          '----------------------------------------^----------------------------------------',
+          '…                                                                                ',
+          '…                                                                                '
+        ].map(line => line.trimEnd()).join('\n'))
+      }
+
+      {
+        const json = `
+        {
+          "project": {
+            "name": "atom-monkey",
+            "description": "If the monkey can do it, you don't have to :P\n\nThe plugin will add the following functionalities ... bla bla bla ...",
+            "tags": ["atom", "plugin", "uuid", "date", "iso", "json", "validate", "format"]
+          },
+          "ok": false
+        }`
+        console.log('json\n' + json)
+        const { success, error, rowPosition, focusBox } = formatJson(json)
+        expect(success).toBe(false)
+        expect(error).toEqual('Unexpected token \n in JSON at position 142')
+        expect(rowPosition).toBeTruthy()
+        expect(rowPosition.row).toEqual(6)
+        expect(rowPosition.index).toEqual(-1) // this is not so good but the error is in the empty line :(
+        console.log('focusBox\n' + focusBox)
+
+        expect(focusBox).toEqual([
+          '            "name": "atom-monkey",',
+          '            "description": "If the monk…',
+          '',
+          '^---------------------------------------',
+          'The plugin will add the following funct…',
+          '            "tags": ["atom", "plugin", …'
+        ].join('\n'))
+      }
     })
   })
 })
